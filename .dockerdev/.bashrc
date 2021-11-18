4f2fe87273325e8beed02dbf61f889b6ec86ab96
Tigran Airapetian <aytigra@gmail.com>
2021-11-18 11:16:11 +0300

    Relieve docker pains

--- .dockerdev/.bashrc
+++
@@ -1,1 +0,0 @@
-alias mtt="mix test"
--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :ozfarium, Ozfarium.Repo,
   username: "postgres",
   password: "postgres",
   database: "ozfarium_dev",
-  hostname: "postgres",
+  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 

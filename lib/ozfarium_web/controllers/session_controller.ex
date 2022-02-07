26fb7bbf8a298183230682a0648b18f2de3c0ea9
Tigran Airapetian <aytigra@gmail.com>
2022-02-07 11:00:24 +0200

    Do not bypass auth

--- lib/ozfarium_web/controllers/session_controller.ex
+++ lib/ozfarium_web/controllers/session_controller.ex
@@ -2,7 +2,7 @@ defmodule OzfariumWeb.SessionController do
   use OzfariumWeb, :controller
 
   def new(conn, _params) do
-    render(conn |> put_session(:current_user_id, 1))
+    render(conn)
   end
 
   def delete(conn, _params) do
